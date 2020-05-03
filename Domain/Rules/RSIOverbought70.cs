a34e21a4d685021136bba79175a0a1194cf32580
emandere <emandere@gmail.com>
2020-05-03 02:54:41 -0400

    Added RSIOverbought rules

---
+++ Domain/Rules/RSIOverbought70.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using System.Linq;
+using forex_app_service.Domain.Indicators;
+namespace forex_app_service.Domain.Rules
+{
+    public class RSIOverbought70:IRule
+    {
+        public bool IsMet(IEnumerable<ForexDailyPrice> window,double currprice)
+        {
+            if(Stats.RSI(window.Select(z=> new List<double>{z.Open,z.Close})) > 70 )
+                return true;
+            else
+                return false;
+        }
+    }
+}
